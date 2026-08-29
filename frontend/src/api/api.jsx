1e3e797bc5b2607b082b2b2e5a02e8283196c124
Mohs Asad <asadkhan28160@gmail.com>
2026-08-29 12:34:49 +0530

    change env

--- frontend/src/api/api.jsx
+++ frontend/src/api/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000", // Aapka backend URL
+  baseURL: import.meta.env.VITE_API_URL, // Aapka backend URL
 });
 
 //  REQUEST INTERCEPTOR (Isse Token Har Request Me Automatic Jayega)
